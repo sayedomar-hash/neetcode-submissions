253ed8c77946650ae07cad39ba3d816276796046
sayedomar-hash <sayed91103@gmail.com>
2026-06-28 23:53:38 +0600

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int s = nums[0];
+        int f = nums[0];
+
+        s = nums[s];
+        f = nums[nums[f]];
+
+        while (s != f)
+        {
+            s = nums[s];
+            f = nums[nums[f]];
+        }
+
+        s = nums[0];
+
+        while (s != f) 
+        {
+            s = nums[s];
+            f = nums[f];
+        }
+
+        return s;
+    }
+};
