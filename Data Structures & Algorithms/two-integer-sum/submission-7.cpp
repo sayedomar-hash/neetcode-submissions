19634e7ebef6098c01ccc38e3338e4ac00b53efa
sayedomar-hash <sayed91103@gmail.com>
2026-07-03 00:04:35 +0600

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mp;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            int r = target - nums[i];
+            if(mp.count(r))
+            {
+                return{mp[r], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+    return{};
+    }
+};
